8c2f3d5d7411aaabcb483c88d83a0e9599179c7e
Nick Rucinski <tur45021@CIS-Linux2.temple.edu>
2023-08-30 14:06:54 -0400

    added comments to random.c

--- random.c
+++ random.c
@@ -3,5 +3,8 @@
 #include "random.h"
 
 char randchar(){
+    //Creates a random number using the rand() function.
+    //The upper bounds is set to 90 which is Z in ascii 
+    //and the lower bounds is set to A which is 65
     return (rand() % (90 - 65 - 1)) + 65;
 }
